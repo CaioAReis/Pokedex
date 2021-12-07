5bd77dc786ae8d77a9b02da9080a359d65c1c297
Caio AReis <Caio.almeida16.ca@gmail.com>
2021-12-07 20:12:47 -0200

    Definindo as imagens do projeto

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import { Fragment } from 'react/cjs/react.production.min';
 
-import { api } from './services/pokeAPI'
+import { kantoDex } from './services/pokeAPI'
 
 function App() {
 
-  // const [pkm, setpkm] = useState({});
+  const [pkm, setpkm] = useState(null);
 
   useEffect(() => {
-    api.getPokemonList();
+    const kanto = async () => {
+      let list = await kantoDex();
+      setpkm(list);
+      // console.log(list);
+    }
+    kanto();
   }, []);
 
   return (
     <Fragment>
+      {
+       pkm === null ? null : pkm.map(pk => (
+          <div key={pk.id}>
+            <h1>#00{pk.id}</h1>
+            <h1>{pk.name}</h1>
+            {pk.types.map(item => (
+              <p>{item.type.name}</p>
+            ))}
+            <img src={pk.image} alt={pk.name} />
+          </div>
+       ))
+      }
       {/* <h1>{pkm.name}</h1>
       <p>{pkm.description}</p>
       <img src={pkm.image} alt={pkm.name} /> */}
--- src/services/pokeAPI.js
+++ src/services/pokeAPI.js
@@ -1,51 +1,71 @@
-const species = 'https://pokeapi.co/api/v2/pokemon-species/';
-// const image = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/';
+const specie = 'https://pokeapi.co/api/v2/pokemon/';
+const image = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/';
 
-const searchPokemonList = async (dexStart, dexEnd) => {
-    const prom = []
+const fetchPokemon = async (id, descID) => {
+    let pokemon = await fetch(`${specie + id}/`)
+        .then(response => response.json());
+    const pokeImage = `${image + id}.png`;
+    return pokemon = {
+        ...pokemon, 
+        image: pokeImage, 
+    };
+}
+
+const searchPokemonList = async (dexStart, dexEnd, descID) => {
+    const list = []
     for (let i = dexStart; i <= dexEnd; i++) {
-        prom.push(fetch(species + i).then(response => response.json()));
+        list.push(await fetchPokemon(i, descID));
     }
-
-    Promise.all(prom).then(pkm => {console.log(pkm)});
-    
+    return list;
 }
 
-export const api = {
-    getPokemonList: async () => {
-        return [
-            {
-                region: 'kanto',
-                pokemons: searchPokemonList(1, 7)
-            },
-            {
-                region: 'johto',
-                pokemons: []
-            },
-            {
-                region: 'hoenn',
-                pokemons: []
-            },
-            {
-                region: 'sinnoh',
-                pokemons: []
-            },
-            {
-                region: 'unova',
-                pokemons: []
-            },
-            {
-                region: 'kalos',
-                pokemons: []
-            },
-            {
-                region: 'alola',
-                pokemons: []
-            },
-            {
-                region: 'galar',
-                pokemons: []
-            },
-        ];
-    }
+const kantoDex = async () => {
+    // 151
+    return await searchPokemonList(1, 9);
 }
+
+const johtoDex = async () => {
+    // 251
+    return await searchPokemonList(152, 160);
+}
+
+const hoennDex = async () => {
+    // 386
+    return await searchPokemonList(252, 261);
+}
+
+const sinnohDex = async () => {
+    // 493
+    return await searchPokemonList(387, 396);
+}
+
+const unovaDex = async () => {
+    // 649
+    return await searchPokemonList(494, 503);
+}
+
+const kalosDex = async () => {
+    // 721
+    return await searchPokemonList(650, 759);
+}
+
+const alolaDex = async () => {
+    // 809
+    return await searchPokemonList(722, 731);
+}
+
+const galarDex = async () => {
+    // 898
+    return await searchPokemonList(810, 819);
+}
+
+export {
+    kantoDex,
+    johtoDex,
+    hoennDex,
+    sinnohDex,
+    unovaDex,
+    kalosDex,
+    alolaDex,
+    galarDex
+};
